bbb66890d0b963133736cab5a82e5f2339980b99
MDias <maildomarcio@gmail.com>
2015-10-22 23:28:10 +0000

    Part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,21 +3,27 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.reduce(:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  arr.max(2).reduce(:+) 
 end
 
+
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+ return false if arr.empty?
+ combinations_found = arr.combination(2).to_a.select { |o| o.inject(:+) == n }
+ combinations_found.size > 0
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, " << name
 end
 
 def starts_with_consonant? s
