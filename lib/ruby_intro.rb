289af48668c493b807ab214f74f7b451e03c84b9
MDias <maildomarcio@gmail.com>
2015-10-27 23:44:03 +0000

    Part 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -22,16 +22,16 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
   "Hello, " << name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s[0] =~ /[a-z&&[^aeiou]]/i
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s = s.to_i(2) if s.match(/^\d+$/)
+  s % 4 == 0
 end
 
 # Part 3
